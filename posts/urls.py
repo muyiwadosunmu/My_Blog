868adf8ac9a84f49e9255e9abe49028f3e5d3063
Muyiwa <oluwamuyiwadosunmu@gmail.com>
2022-09-21 01:11:17 +0100

    Adding structures

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+
+from posts.models import Post
 from . import views
+from . views import PostListView
 
 urlpatterns = [
-    path('', views.home, name="blog-home")
+    path('', PostListView.as_view(), name="blog-home"),
+    path('about/', views.about, name="blog-about")
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from . models import Post
+from django.views.generic import ListView
 
 # Create your views here.
 
@@ -7,3 +8,14 @@ def home(request):
     posts = Post.objects.all()
     context = {"posts": posts}
     return render(request, "posts/home.html", context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = "posts/home.html"
+    context_object_name: "posts"
+
+    #<app>/<model>_<view_type>.html
+
+def about(request):
+    return render(request, "posts/about.html", {'title':"About"})
